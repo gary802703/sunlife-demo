622c71ea33a59203863c8554b269ae6f2aa2ca0d
gary802703 <50373196+gary802703@users.noreply.github.com>
2025-04-21 15:36:47 +0800

    Revert "Deleted"
    This reverts commit 972ed26165f5ebeff9837779a7858e5ef09adcc3.

---
+++ Source.ps1
@@ -0,0 +1,15 @@
+this is testing source code for git exam.
+1. Create
+2. Compile 
+3. Deploy
+4. Push it
+5. Completed
+6. Release to customer
+7. Approval to clients
+8. UAT testing phase
+9. Add one user item
+10 User report 2 issue
+11 Missing for branch v1.1
+12 Add Branch in version 1.2
+13 Updated B01 Branch
+14 Updated A01 Branch
